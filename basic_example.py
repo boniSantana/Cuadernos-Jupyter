bc2d299bff79571f55c27bc4f166d48a2ad3d56d
Federico Santana <fede.santana@hotmail.com>
2019-10-04 16:46:08 -0300

    Se arreglaron las líneas en el 0. Con un pequeño truco aún se mantienen los laterales, agregando al MIN y MAX 0.01.

--- basic_example.py
+++ basic_example.py
@@ -15,8 +15,12 @@ import plotly.tools as tls
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure(num=None, figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
 
-XMAX = 10
-ax = plt.axes(xlim=(0, XMAX), ylim=(-0.1, 10))
+XMAX = 20
+MAXVALUEFUNC = 7
+desplazamiento = XMAX - MAXVALUEFUNC
+MINVALUEFUNC = 3
+
+ax = plt.axes(xlim=(0, XMAX), ylim=(-0.1, 50))
 #eje_x = [1,2,3,4,5,6,7,8,9,10]
 #my_xticks = ['t', 't-1', 't-2', 't-3', 't-4', 't-5', 't-6', 't-7', 't-8', 't-9']
 #plt.xticks(eje_x, my_xticks)
@@ -31,24 +35,23 @@ def init():
 
 # animation function.  This is called sequentially
 def animate(t):
-    
-    x: np.ndarray = np.linspace(0, XMAX, 1000)
+
+    x: np.ndarray = np.linspace(MINVALUEFUNC-0.01, MAXVALUEFUNC+0.01, 1000)
     z: np.ndarray = np.copy(x)
-    y: np.ndarray = np.piecewise(x, [x<3, (x>=3) & (x<=7), x>7], [lambda x: 0,lambda x: x , lambda x: 0]) 
-    z: np.ndarray = z + ((t/100)%3)
-    print("Z =", z[-1])
-    if (z[-1] >= XMAX+3) : 
-        z = np.copy(x)
-        print (z[-1])
+    y: np.ndarray = np.piecewise(x, [x<MINVALUEFUNC, (x>=MINVALUEFUNC) & (x<=MAXVALUEFUNC), x>MAXVALUEFUNC], [lambda x: 0,lambda x: x**2 , lambda x: 0]) 
+    z: np.ndarray = z + (t/100)
     
+    if (z[-1] >= XMAX+XMAX-MAXVALUEFUNC) : 
+        z = np.copy(x)
+
     line.set_data(z, y)
-    print (t)
+
     return line,
 
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=1000, interval=10, blit=True)
+                               frames= desplazamiento * 100, interval=10, blit=True)
 
 # save the animation as an mp4.  This requires ffmpeg or mencoder to be
 # installed.  The extra_args ensure that the x264 codec is used, so that
