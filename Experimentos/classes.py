70effac701e8fbe4cad24fa54a404b18ff63f08e
Santiago Santana <santana.santiago@gmail.com>
2019-10-05 22:32:02 -0300

    formatting

--- Experimentos/classes.py
+++ Experimentos/classes.py
@@ -1,41 +1,42 @@
+from matplotlib import animation
+import matplotlib.pyplot as plt
+import numpy as np
 import matplotlib
 
 matplotlib.use('Qt5Agg')
 
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import animation
-import plotly.tools as tls
-
 class function:
 
-    def __init__(self, conditionArray,funcArray, Xfunc):
+    def __init__(self, conditionArray, funcArray, Xfunc):
         self.conditionArray = conditionArray
         self.funcArray = funcArray
         self.Xfunc = Xfunc
-        self.x = np.linspace(self.Xfunc[0]-0.01, self.Xfunc[1]+0.01, self.Xfunc[2])
+        self.x = np.linspace(
+            self.Xfunc[0]-0.01, self.Xfunc[1]+0.01, self.Xfunc[2])
 
     def __str__(self):
         return str("Aca voy a poner en el JupyterNotebook algo cool para mostrar tipo Latex la ec")
 
     def Xvalues(self):
         return self.x
-    
+
     def Condition(self):
-        return [self.x<self.Xfunc[0], (self.x>=self.Xfunc[0]) & (self.x<=self.Xfunc[1]), self.x>self.Xfunc[2]]
+        return [self.x < self.Xfunc[0], (self.x >= self.Xfunc[0]) & (self.x <= self.Xfunc[1]), self.x > self.Xfunc[2]]
 
     def Function(self):
-        return [lambda f:self.funcArray[0],lambda f:self.funcArray[1],lambda f:self.funcArray[2]]
+        return [lambda f:self.funcArray[0], lambda f:self.funcArray[1], lambda f:self.funcArray[2]]
 
     def Yvalues(self):
-        return np.piecewise(self.x, self.Condition(), self.Function()) 
+        return np.piecewise(self.x, self.Condition(), self.Function())
+
 
 # First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure(num=None, figsize=(14, 6), dpi=80, facecolor='w', edgecolor='k')
+fig = plt.figure(num=None, figsize=(14, 6), dpi=80,
+                 facecolor='w', edgecolor='k')
 
 
-movefunction = function ([2,11],[0, 1, 0], [2,4,1000])
-staticfunction = function ([7.5,11], [0, 10, 0], [7.5, 11, 1000])
+movefunction = function([2, 11], [0, 1, 0], [2, 4, 1000])
+staticfunction = function([7.5, 11], [0, 10, 0], [7.5, 11, 1000])
 
 XMAX = 20
 MAXVALUEFUNC = 4
@@ -51,39 +52,46 @@ ax = plt.axes(xlim=(0, XMAX), ylim=(-0.1, 50))
 #plt.xticks(eje_x, my_xticks)
 
 line, = ax.plot([], [], lw=2)
-line2, = ax.plot ([], [], lw=2)
+line2, = ax.plot([], [], lw=2)
 
 x2 = np.linspace(MINVALUEFUNC2-0.01, MAXVALUEFUNC2+0.01, 1000)
-y2 = np.piecewise(x2, [x2<MINVALUEFUNC2, (x2>=MINVALUEFUNC2) & (x2<=MAXVALUEFUNC2), x2>MAXVALUEFUNC2], [lambda x: 0,lambda x: 2*x , lambda x: 0]) 
-polygone = ax.fill_between (x2[0:0] ,y2[0:0], facecolor='yellow', alpha=0.5)
-   
+y2 = np.piecewise(x2, [x2 < MINVALUEFUNC2, (x2 >= MINVALUEFUNC2) & (
+    x2 <= MAXVALUEFUNC2), x2 > MAXVALUEFUNC2], [lambda x: 0, lambda x: 2*x, lambda x: 0])
+polygone = ax.fill_between(x2[0:0], y2[0:0], facecolor='yellow', alpha=0.5)
+
 # initialization function: plot the background of each frame
+
+
 def init():
     line.set_data([], [])
     line2.set_data([x2], [y2])
     return line, line2, polygone
 
 # animation function.  This is called sequentially
+
+
 def animate(t):
 
     x = movefunction.Xvalues()
     z = np.copy(x)
-    y = movefunction.Yvalues() 
+    y = movefunction.Yvalues()
     z = z + (t/100)
 
     x2: np.ndarray = np.linspace(MINVALUEFUNC2-0.01, MAXVALUEFUNC2+0.01, 1000)
-    
+
     ax.collections.clear()
-     
-    #348 
+
+    # 348
     if (z[-1] >= x2[0]):
-        polygone = ax.fill_between (x2[0:2*(t-348)] ,y2[0:2*(t-348)], facecolor='yellow', alpha=0.5)
+        polygone = ax.fill_between(
+            x2[0:2*(t-348)], y2[0:2*(t-348)], facecolor='yellow', alpha=0.5)
 
     else:
-        polygone = ax.fill_between (x2[0:0] ,y2[0:0], facecolor='yellow', alpha=0.5)
+        polygone = ax.fill_between(
+            x2[0:0], y2[0:0], facecolor='yellow', alpha=0.5)
 # sin el else no anda, se referencia antes de que se llame, no entiendo por que.
 
-    if (z[-1] >= XMAX+XMAX-MAXVALUEFUNC) : 
+    if (z[-1] >= XMAX+XMAX-MAXVALUEFUNC):
         z = np.copy(x)
 
     line.set_data(z, y)
@@ -92,7 +100,7 @@ def animate(t):
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames= desplazamiento * 100, interval=10, blit=True)
+                               frames=desplazamiento * 100, interval=10, blit=True)
 
 # save the animation as an mp4.  This requires ffmpeg or mencoder to be
 # installed.  The extra_args ensure that the x264 codec is used, so that
@@ -103,23 +111,23 @@ anim = animation.FuncAnimation(fig, animate, init_func=init,
 
 plt.show()
 
-#Si ymove>yquieto pinto yquieto,
-#Sino pinto ymove
-#Mientras haya intersección:
-#Si ymove>yquieto pinto yquieto,
-#Sino pinto ymove
-#Una variable que guarde un array de condiciones y otro un array de funciones
-#Y un intervalo donde graficar en X la función
-#Una clase llamada funcion que le pasas las condiciones y las funciones y las escribe correctamente
-#Adentro tiene el piecewise directamentee
-#Los parametros para inicializarla son:
-#1. Un np.ndarray de valores de X.
-#2. Un array de funciones
-#3. Un array de condiciones
+# Si ymove>yquieto pinto yquieto,
+# Sino pinto ymove
+# Mientras haya intersección:
+# Si ymove>yquieto pinto yquieto,
+# Sino pinto ymove
+# Una variable que guarde un array de condiciones y otro un array de funciones
+# Y un intervalo donde graficar en X la función
+# Una clase llamada funcion que le pasas las condiciones y las funciones y las escribe correctamente
+# Adentro tiene el piecewise directamentee
+# Los parametros para inicializarla son:
+# 1. Un np.ndarray de valores de X.
+# 2. Un array de funciones
+# 3. Un array de condiciones
 
-#Los metodos son:
-#yvalues()
-#xvalues()
+# Los metodos son:
+# yvalues()
+# xvalues()
 #
 #
 #
