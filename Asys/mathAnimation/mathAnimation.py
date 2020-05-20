51e5d787f4f0b6db154e120cf9890aec0cc57e0f
Federico Santana <fede.santana@hotmail.com>
2020-05-20 00:52:51 -0300

    Funciona slider

--- Asys/mathAnimation/mathAnimation.py
+++ Asys/mathAnimation/mathAnimation.py
@@ -6,7 +6,6 @@ website: http://jakevdp.github.com
 license: BSD
 """
 import matplotlib
-matplotlib.use ('cairo')
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
@@ -29,33 +28,28 @@ class Plot:
 
         self.y = 2/3 * pow(np.e, (-1/2)*self.x) * np.cos((pow(3, 0.5)/2) * self.x) - (2/3) * pow(np.e, -2*self.x)
         
-        self.firsTime = True
+        self.animation = animation.FuncAnimation(self.fig, self.update,self.endPoint - self.currentFrame - 2, interval=10, repeat_delay=5000, repeat=False, blit=False)
+        
 
     def set_value (self, frame=0):
         frame = int(frame)
         self.currentFrame = frame  ##  el valor del slider al de la animación
-        if (self.firsTime == False):
-            self.animation.event_source.stop()
-        else:
-            self.firsTime = True
         print("entre")
-        self.animation = animation.FuncAnimation(self.fig, self.update, 99, interval=10, repeat_delay=5000, repeat=True, blit=False)
+        self.animation.event_source.stop()
+        self.animation = animation.FuncAnimation(self.fig, self.update, self.endPoint - self.currentFrame - 2, interval=10, repeat_delay=5000, repeat=False, blit=False)
 
-    def update(self, frame):
-        frame = self.currentFrame
-        '''No deberían ser necesarias estas 3 lineas, pero no se resetea adecuadamente la animación'''
-        if (frame == 1000): # si termino
-            self.animation.event_source.stop()
-            return self.line, self.point
-            
-        else:
-            self.line.set_data(self.x[0:frame], self.y[0:frame]) # actualizar valor line
-            self.point.set_data(self.x[frame],self.y[frame]) # actualizar valor point
-            # self.slider.set_val(frame) # actualizar valor slider # Si lo descomento entra constantemente a set_value, cosa que esta MAL
-            self.currentFrame += 1 # siguiente frame
-            print("currentFrame", self.currentFrame)
-            print("Despues", frame)
-            return self.line, self.point
+    def update(self, frame):       
+        frame = self.currentFrame     
+        self.line.set_data(self.x[0:frame], self.y[0:frame]) # actualizar valor line
+        self.point.set_data(self.x[frame],self.y[frame]) # actualizar valor point
+        self.slider.eventson = False # Que no me reconozca mas los eventos
+        self.slider.set_val(frame) # cambio el valor
+        self.slider.eventson = True # Que los vuelva a reconocer        
+        self.currentFrame += 1 # siguiente frame
+        print("currentFrame", self.currentFrame)
+        print("Despues", frame)
+
+        return self.line, self.point
     
     def animateSlider(self):
         slider_ax = self.fig.add_axes((0.1, 0.025, 0.5, 0.04))
@@ -68,10 +62,9 @@ class Plot:
         plt.show()
     
     def save(self):
-        self.animation = animation.FuncAnimation(self.fig, self.update, 99, interval=10, repeat_delay=5000, repeat=True, blit=False)
         self.animation.save('animation.gif', writer='imagemagick', fps=60)
 
-    
+
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
